fadd29871e4581e3436d3e56d5ab418e86fb29ce
alena <pylkaya@ya.ru>
2021-09-05 18:16:54 +0300

    feat: complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.lowerNumber = min;
+        this.higherNumber = max;
     }
 
     guess() {
-
+        this.result = Math.ceil((this.lowerNumber + this.higherNumber) / 2);
+        return this.result;
     }
 
     lower() {
-
+        this.higherNumber = this.result;
     }
 
     greater() {
-
+        this.lowerNumber = this.result;
     }
 }
 
